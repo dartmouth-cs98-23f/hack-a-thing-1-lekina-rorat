b502961c4462c3421eb0d6f4d532bdec6909bc55
Mark Lekina Rorat <marklekina@gmail.com>
2023-09-16 11:56:00 -0400

    complete backend

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,12 +1,13 @@
-using BackEnd;
+﻿using BackEnd.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
    ?? "Data Source=conferences.db";
 
-builder.Services.AddSqlite<BackEnd.Models.ApplicationDbContext>(connectionString);
-
+builder.Services.AddSqlite<BackEnd.Data.ApplicationDbContext>(connectionString);
+    
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,30 +24,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapSpeakerEndpoints();
+app.MapAttendeeEndpoints();
+app.MapSessionEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
